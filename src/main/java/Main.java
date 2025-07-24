a14fb7af637c162bfafc929d08afed99f59ffc7a
vaibhavhapani <vaibhavhapani84@gmail.com>
2025-07-25 02:00:19 +0530

    Implement the ECHO command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,16 +30,50 @@ public class Main {
     }
 
     private static void handleClient(Socket clientSocket) throws IOException {
-        InputStream in = clientSocket.getInputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         OutputStream out = clientSocket.getOutputStream();
 
-        byte[] buffer = new byte[1024];
-        while(true){
-            int read = in.read(buffer);
-            if(read == -1) break;
+        String line;
+        while((line = in.readLine()) != null){
+            if(line.startsWith("*")) {
+                int arrayLength = Integer.parseInt(line.substring(1));
+                List<String> command = new ArrayList<>();
 
-            out.write("+PONG\r\n".getBytes());
-            out.flush();
+                for(int i = 0; i < arrayLength; i++) {
+                    String newLine = in.readLine();
+                    if(newLine != null && newLine.startsWith("$")){
+                        int commandLength = Integer.parseInt(newLine.substring(1));
+                        if(commandLength >= 0){
+                            String element = in.readLine();
+                            if(element != null) command.add(element);
+                        }
+                    }
+                }
+
+                if(!command.isEmpty()){
+                    String commandName = command.get(0).toUpperCase();
+
+                    switch (commandName) {
+                        case "PING":
+                            out.write("+PONG\r\n".getBytes());
+                            break;
+                        case "ECHO":
+                            if(command.size() > 1) {
+                                String arg = command.get(1);
+                                String response = "$" + arg.length() + "\r\n" + arg + "\r\n";
+                                out.write(response.getBytes());
+                            } else {
+                                out.write("-ERR wrong number of arguments for 'echo' command\r\n".getBytes());
+                            }
+                            break;
+                        default:
+                            out.write(("-ERR unknown command '" + commandName + "'\r\n").getBytes());
+                            break;
+                    }
+                    out.flush();
+                }
+            }
         }
+        out.flush();
     }
 }
