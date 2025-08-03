08b19329bd00bcc8663af5a82e35f1152fd08f91
vaibhavhapani <vaibhavhapani84@gmail.com>
2025-08-03 20:11:36 +0530

    Initial replication ID and offset

--- src/main/java/com/redis/server/command/CommandHandlers.java
+++ src/main/java/com/redis/server/command/CommandHandlers.java
@@ -487,9 +487,7 @@ public class CommandHandlers {
 
         if(isReplica) writeBulkString("role:slave", out);
         else {
-            // writeBulkString("role:master", out);
-            writeBulkString("master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb", out);
-            writeBulkString("master_repl_offset:0", out);
+            writeBulkString("role:master\r\nmaster_repl_offset:0\r\nmaster_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb", out);
         }
     }
 }
