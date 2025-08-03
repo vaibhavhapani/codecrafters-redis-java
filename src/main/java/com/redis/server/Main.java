fa055b324f21497de46ff70ac27dcf9826e90701
vaibhavhapani <vaibhavhapani84@gmail.com>
2025-08-03 17:54:51 +0530

    Configure listening port

--- src/main/java/com/redis/server/Main.java
+++ src/main/java/com/redis/server/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int len = args.length;
         int port = RedisConstants.DEFAULT_PORT;
+
+        for(String s: args) System.out.println(s);
         if(len > 0 && args[0] == "port") port = Integer.parseInt(args[1]);
 
         RedisServer server = new RedisServer(port);
